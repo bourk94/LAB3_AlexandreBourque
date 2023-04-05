1eb42687b2e68f7893eb52e46b6a8e9545220b19
bourk94 <113631191+bourk94@users.noreply.github.com>
2023-04-04 23:19:31 -0400

    Creation start

---
+++ Assets/_MyAssets/Scripts/Gestionaire/GestionScene.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GestionScene : MonoBehaviour
+{
+    [SerializeField] private GameObject _menuInstructions = default;
+    public void DemarrerJeu()
+    {
+        int noScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(noScene + 1);
+    }
+
+    public void QuitterJeu()
+    {
+        Application.Quit();
+    }
+
+    public void RetourMenuPrincipal()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OuvrirMenuInstruction()
+    {
+        if (_menuInstructions.activeSelf)
+        {
+            _menuInstructions.SetActive(false);
+        }
+        else
+        {
+            _menuInstructions.SetActive(true);
+        }
+    }
+}
